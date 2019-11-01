729b5ae4bd15b02696edf28073e4ac80b9b2e402
antonS <so4usertrue@gmail.com>
2019-11-01 21:36:22 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(() => {
+    $('.works__btns button, .works__btns a').on('focus', ({ target }) => {
+        $(target).parents('.works__element').addClass('works__element--active');
+    });
+
+    $('.works__btns button, .works__btns a').on('blur', ({ target }) => {
+        $(target).parents('.works__element').removeClass('works__element--active');
+    });
+
+    function moveProgressBar(node, nodeLine, tooltip, animationLength = 1500) {
+        const progressElement = $(node);
+        progressElement.each(function (value, item) {
+            $(item).find(nodeLine).animate({
+                width: item.dataset.progressPercent + '%',
+            }, animationLength);
+
+            $(item).find(tooltip).show(animationLength);
+        })
+    }
+
+    let animate = true;
+
+    $(window).scroll(function() {
+
+        if (animate && $('.skills').offset().top <= $(window).scrollTop() + 150) {
+            moveProgressBar('.progress__element', '.progress__line', '.progress__tooltip');
+            animate = false;
+        }
+    });
+
+});
